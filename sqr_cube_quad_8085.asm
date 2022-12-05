969eb2c69fe93f838fdb11df3763e3361bbf7393
Subash_1237 <77325527+subash1237@users.noreply.github.com>
2022-12-05 23:01:47 +0545

    create_sqr_cube_quad_8085.asm
    IT is a program written in assembly language with processor articture 8085 intel, used to calculate square ,cube ... of any number answer must not be exceeding FFFF  HEX or 65535 decimal.

---
+++ sqr_cube_quad_8085.asm
@@ -0,0 +1,53 @@
+
+; REMEMBER 
+; SET 20001 with value of  number to be powerred and SET 2004 add with value of power.
+; RESET OTHER unnecessary MEMORY BEFORE EXECUTION.
+; REMEMBER TO SET MEMORY LOC TO 2000H; series.
+;ANSWER WILL BE DISPLAYED ON 2010H and 2011H; 
+
+MVI A,00H;
+STA 2010h;
+LDA 2004H;
+MOV C,A;
+LDA 2000H;
+MOV B,A;
+DCR B;
+eis0:DCR C;
+JZ last;
+STA 20A0H;
+LDA 2000H;
+MOV E,A;
+DCR E;
+LDA 20A0H;
+MOV D,A;
+feriadd:ADD D;
+JNC NOcarry;
+
+STA 20A0H; store a to 2010H;
+MOV A,E;
+CMP B;
+JNZ aaileaddnagarne;
+
+LDA 2000H; load a from 2000h to give to d
+MOV L,A;
+DCR L;
+LDA 2010H; load a from 2010H;
+MOV H,A;
+ADDcarryuntil:ADD H;
+DCR L;
+JNZ ADDcarryuntil;
+STA 2010H; store it to same location.
+
+
+aaileaddnagarne: LDA 2010H;
+INR A;
+STA 2010H;
+LDA 20A0H;
+
+
+NOcarry:DCR E;
+JZ eis0; jump if E is 0;
+JNZ feriadd;
+
+last:STA 2011H; store a last value 
+hlt;
